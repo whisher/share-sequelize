1dd077b7f72bc44deb949f45001c8742f3126f60
whisher <whisher06@gmail.com>
2015-10-08 20:57:46 +0200

    set up sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -8,11 +8,10 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 console.log(config.use_env_variable);
-
 if (config.use_env_variable) {
-  var sequelize = new Sequelize('postgres://rentatwo:rentatwo@localhost:5432/rentatwo');
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize('postgres://rentatwo:rentatwo@localhost:5432/rentatwo');
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
